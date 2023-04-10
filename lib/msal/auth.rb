9733ca3e3e75e9e5dbff46056d77c368abaf54a0
amin-abbasiy <uamin.en@gmail.com>
2023-04-10 21:46:55 +0100

    complete url creation

---
+++ lib/msal/auth.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+module Msal
+    class Auth
+        MANDATORY_PARAMS = [:client_id, :scope]
+
+        attr_accessor :code
+
+        def initialize(payload = {})
+            @client_id = payload[:client_id]
+            @tenant = payload[:tenant] || 'common'
+            @response_type = payload[:response_type] || 'code'
+            @response_mode = payload[:response_mode] || 'query'
+            @scope = payload[:scope]
+            @redirect_uri = payload[:redirect_uri]
+            @state = payload[:state]
+            @url = "https://login.microsoftonline.com/#{@tenant}/oauth2/v2.0/authorize"
+
+            # raise ::ArgumentError => e if validate_params(payload)
+        end
+
+        def authorize_uri
+            CGI.escape("#{@url}?"                           +
+                         "client_id=#{@client_id}"          +
+                         "&response_type=#{@response_type}" +
+                         "&redirect_uri=#{@redirect_uri}"   +
+                         "&response_mode=#{@response_mode}" +
+                         "&scope=#{@scope}&state=#{@state}")
+        end
+
+
+        private
+        def validate_params(payload)
+            payload.each {|k, v| MANDATORY_PARAMS.include?(k) && k[v].nil? }
+        end
+    end
+end
---
+++ spec/msal/auth_spec.rb
@@ -0,0 +1,59 @@
+# frozen_string_literal: true
+
+require_relative '../spec_helper'
+require_relative '../../lib/msal/auth'
+
+RSpec.describe Msal::Auth do
+  let(:response) {
+
+  }
+
+  let(:error_in_response) {
+    {
+      error: { code: "Forbidden" }
+    }
+  }
+
+  let(:payload) {
+    {
+      client_id: '11111111-1111-1111-1111-111111111111',
+      tenant: 'common',
+      response_type: 'code',
+      response_mode: 'query',
+      scope: 'offline_access openid profile',
+      redirect_uri: 'http://localhost/myapp',
+      state: '11111'
+    }
+  }
+
+  let(:authorization_uri) { CGI.escape("https://login.microsoftonline.com/common/oauth2/v2.0/authorize?" +
+                                         "client_id=11111111-1111-1111-1111-111111111111" +
+                                         "&response_type=code" +
+                                         "&redirect_uri=http://localhost/myapp&response_mode=query" +
+                                         "&scope=offline_access openid profile" +
+                                         "&state=11111") }
+
+  it "authorization uri" do
+    auth = ::Msal::Auth.new(payload)
+    expect(auth.authorize_uri).to eq(authorization_uri)
+  end
+
+  it "return AuthorizationError if user did not grant permission" do
+    expect { ::Msal::Response.new(error_in_response) }.to raise_error(AuthorizationError)
+  end
+
+  it "token request response"
+  it "RequestError for token failed attempt"
+
+
+  xit 'return access code from request' do
+    stub_request(:post, 'https://login.microsoftonline.com/common/oauth2/v2.0/token')
+      .with(body: payload)
+      .to_return(status: 200, body: 'codecode')
+
+    auth = ::Msal::Auth.new(payload)
+    data = auth.code
+
+    expect(data).to eq('codecode')
+  end
+end
