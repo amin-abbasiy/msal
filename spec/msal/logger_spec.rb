e4cf207de8fee8ae19676ae3bead874074234dc6
Amin AB <uamin.en@gmail.com>
2023-07-06 23:25:12 +0200

    add logger and test cases

---
+++ spec/msal/logger_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require_relative '../../lib/msal/logger'
+
+RSpec.describe ::Msal::MsalLogger do
+  context 'Logger level' do
+    subject { described_class.new($stdout, { level: ::Logger::INFO }) }
+    it 'Info' do
+      allow(subject).to receive(:info)
+      subject.info('message')
+
+      expect(subject.level).to eq(1)
+      expect(subject).to have_received(:info).with('message')
+    end
+  end
+
+  context 'Debug level' do
+    subject { described_class.new($stderr, { level: ::Logger::ERROR }) }
+    it 'Debug' do
+      allow(subject).to receive(:debug)
+      subject.debug('message')
+
+      expect(subject.level).to eq(3)
+      expect(subject).to have_received(:debug).with('message')
+    end
+  end
+end
