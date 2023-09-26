a20ffa641c7abafdf1462b0a0a75662e1594b1b8
amin-abbasiy <uamin.en@gmail.com>
2023-09-26 23:26:34 +0200

    fix offenses

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,7 +8,7 @@ require 'vcr'
 WebMock.disable_net_connect!(allow_localhost: true)
 
 VCR.configure do |config|
-  config.cassette_library_dir = "fixtures/vcr_cassettes"
+  config.cassette_library_dir = 'fixtures/vcr_cassettes'
   config.hook_into :webmock
 end
 
